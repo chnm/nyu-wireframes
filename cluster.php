5eefc8e08790a6c4758bc9e6f88b4bcb5d42d685
Ken Albers <ken.albers@gmail.com>
2018-09-14 11:27:12 -0400

    text

--- cluster.php
+++ cluster.php
@@ -5,18 +5,30 @@
 <div class="grid-x grid-padding-x">
     <div class="medium-4 cell">
         <img src="https://via.placeholder.com/400x400?text=CLUSTER+IMAGE" alt="">
-        <h4>Metadata Title</h4>
-        <p class="metadata">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, quam ad quae excepturi facere iste placeat expedita sunt. Itaque possimus repellat eaque sunt ratione, non quae sint. Placeat, consequuntur, hic.</p>
-        <h4>Metadata Title</h4>
-        <p class="metadata">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, quam ad quae excepturi facere iste placeat expedita sunt. Itaque possimus repellat eaque sunt ratione, non quae sint. Placeat, consequuntur, hic.</p>
-        <h4>Metadata Title</h4>
-        <p class="metadata">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, quam ad quae excepturi facere iste placeat expedita sunt. Itaque possimus repellat eaque sunt ratione, non quae sint. Placeat, consequuntur, hic.</p>
-        <h4>Metadata Title</h4>
-        <p class="metadata">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, quam ad quae excepturi facere iste placeat expedita sunt. Itaque possimus repellat eaque sunt ratione, non quae sint. Placeat, consequuntur, hic.</p>
+        <h4>Effects and Outcomes</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis adipisci, consequuntur placeat, neque ex iste quo facere ea, illum culpa eum ducimus numquam praesentium similique fugit velit doloremque voluptatibus ullam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, obcaecati neque. Facere consequatur animi distinctio cupiditate repellendus, cum ut quidem aliquam quae velit sed, quam reiciendis aut alias maxime ullam!</p>
+        <h4>Authors</h4>
+        <p class="metadata"><a href="#">First Last</a></p>
+        <p class="metadata"><a href="#">First Last</a></p>
+        <p class="metadata"><a href="#">First Last</a></p>
+        <p class="metadata"><a href="#">First Last</a></p>
+        <h4>Links and Resources</h4>
+        <p class="metadata"><a href="#">Link One</a></p>
+        <p class="metadata"><a href="#">Link Two</a></p>
+        <p class="metadata"><a href="#">Link Three</a></p>
+        <p class="metadata"><a href="#">Link Four</a></p>
+        <h4>Tags</h4>
+        <p class="metadata"><a href="#">Tag One</a></p>
+        <p class="metadata"><a href="#">Tag Two</a></p>
+        <p class="metadata"><a href="#">Tag Three</a></p>
+        <p class="metadata"><a href="#">Tag Four</a></p>
+        <h4>Media History</h4>
+        <p class="metadata">The reporting was intended for these media types: Newspaper</p>
     </div>
 
     <div class="medium-8 cell">
         <h3><a href="article.php">Article Title</a></h3>
+        <p class="subhead">Subheading for the Article</p>
         <p>Blurb about this article. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio omnis molestiae voluptates magni vel. Labore doloremque vel placeat iure architecto quidem eos nulla ut tempora, optio veritatis laboriosam iusto rem!</p>
         <h3><a href="article.php">Article Title</a></h3>
         <p>Blurb about this article. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio omnis molestiae voluptates magni vel. Labore doloremque vel placeat iure architecto quidem eos nulla ut tempora, optio veritatis laboriosam iusto rem!</p>
